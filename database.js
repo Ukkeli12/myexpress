5dd8b0e5a4c72aa42f7d7bbfde1e0a5a27670f02
unknown <t0kape02@students.oamk.fi>
2021-02-22 20:19:13 +0200

    Muokattu tietokannan sijaintia

--- database.js
+++ database.js
@@ -5,8 +5,8 @@ const connection = mysql.createPool({
   user: 'netuser',
   password: 'netpass',
   database: 'tvt20spl'
-});*/
-
+});
+*/
 const dotenv = require('dotenv');
 dotenv.config();
 const connection = mysql.createConnection(process.env.JAWSDB_URL);
