7349c763f9c7f8eece5146cb223e675f3a4adb63
unknown <t0kape02@students.oamk.fi>
2021-02-22 19:44:04 +0200

    valmis herokuun

--- database.js
+++ database.js
@@ -1,8 +1,15 @@
 const mysql = require('mysql');
+/*
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'netuser',
   password: 'netpass',
   database: 'tvt20spl'
 });
+*/
+
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 module.exports = connection;
